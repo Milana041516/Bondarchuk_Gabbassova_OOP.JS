5871379ffbfbdb84a59e4bb3abf74823feaecbe9
Milana <milana15.10.05@gmail.com>
2025-03-20 21:16:48 -0400

    Update oop.js functionality
    In this commit the functionality of oop.js was added; classes and subclasses were created.

---
+++ js/modules/Pet.js
@@ -0,0 +1,59 @@
+export class Pet {
+    constructor(name, species, age, image, favoriteToy, owner) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+        this.image = image;
+        this.favoriteToy = favoriteToy;
+        this.owner = owner;
+    }
+
+    displayInfo(container) {
+        const card = document.createElement('div');
+        card.classList.add('pet-card');
+        card.dataset.breed = this.breed;
+        card.innerHTML = 
+        `   <img src="${this.image}" alt="${this.name}">
+            <h2>${this.name}</h2>
+            <p>Species: ${this.species}</p>
+            <p>Age: ${this.age}</p>
+            <p>Favorite Toy: ${this.favoriteToy}</p>
+            <div class="full-profile" style="display: none;"></div>
+        `;
+        container.appendChild(card);
+
+        // Image click triggers showFullProfile()
+        // const img = card.querySelector('.pet-image');
+        // img.addEventListener('click', () => {
+        //     this.showFullProfile(card.querySelector('.full-profile'));
+        // });
+    }
+}
+
+export class Dog extends Pet {
+    constructor(name, species, age, image, favoriteToy, owner) {
+        super(name, species, age, image, favoriteToy, owner);
+        this.breed = "Dog";
+    }
+}
+
+export class Cat extends Pet {
+    constructor(name, species, age, image, favoriteToy, owner) {
+        super(name, species, age, image, favoriteToy, owner);
+        this.breed = "Cat";
+    }
+}
+
+export class SmallAnimal extends Pet {
+    constructor(name, species, age, image, favoriteToy, owner) {
+        super(name, species, age, image, favoriteToy, owner);
+        this.breed = "SmallAnimal";
+    }
+}
+
+export class Bird extends Pet {
+    constructor(name, species, age, image, favoriteToy, owner) {
+        super(name, species, age, image, favoriteToy, owner);
+        this.breed = "Bird";
+    }
+}
