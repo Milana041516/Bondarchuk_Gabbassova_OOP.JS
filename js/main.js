1c570c9850de98108a32292afdc90778acf29a83
Milana <milana15.10.05@gmail.com>
2025-03-21 10:08:59 -0400

    Update oop.js functionality
    In this commit more methods were added to classes for oop.js functionality.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@ import { filter } from './modules/filter.js';
 import { animateCards } from './modules/gsap.js';
 
 const container = document.querySelector('.pets-container');
-
 pets.forEach(pet => {
     pet.displayInfo(container);
 });
 
-animateCards();
+const popupCard = document.querySelector('.popup-card');
+const closeBtn = document.querySelector('.close-btn');
 
+closeBtn.addEventListener('click', () => {
+    popupCard.classList.add('hidden');
+});
+
+
+animateCards();
 filter();
--- js/modules/Owner.js
+++ js/modules/Owner.js
@@ -1,7 +1,15 @@
 export class Owner {
-    constructor (name, image, description) {
+    constructor(name, image, description) {
         this.name = name;
         this.image = image;
         this.description = description;
+    }
+
+    getPopupContent() {
+        return `
+            <img src="images/${this.image}" alt="${this.name}">
+            <h2>${this.name}</h2>
+            <p>${this.description}</p>
+        `;
     }
 }
